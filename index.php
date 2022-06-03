8b9dc84520654e2ae22070440dcbf607dab5613a
ricalva <ricardo.am2000@gmail.com>
2022-06-03 14:35:26 -0500

    pagina ios

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>TRABAJO EN HTML</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilo.css">
+</head>
+<H1> MI PRIMERA PAGINA EN ESTA MATERIA DE FRAMEWORKS WEB</H1>
+
+<body>
+
+  <p>TEMARIO</p>
+  <li>Unidad 1</li>
+  <li>Unidad 2</li>
+  <li>Unidad 3</li>
+  <li>Unidad 4</li>
+  <li>Unidad 5</li>
+
+  <p>Algunos tipos de programacion web:</p>
+
+<p>
+
+<p>Frontend</p>
+
+La parte de un sitio web con la que el usuario interactúa directamente se denomina interfaz. También se conoce como el "lado del cliente" de la aplicación. Incluye todo lo que los usuarios experimentan directamente: colores y estilos de texto, imágenes, gráficos y tablas, botones, colores y menú de navegación. HTML, 
+CSS y JavaScript son los lenguajes utilizados para el desarrollo de Front End. Los desarrolladores front-end implementan la estructura, el diseño, el comportamiento y el contenido de todo lo que se ve en las pantallas del navegador cuando se abren sitios web, aplicaciones web o aplicaciones móviles.
+
+<p>Backend</p>
+
+El backend es el lado del servidor del sitio web. Almacena y organiza los datos, y también se asegura de que todo en el lado del cliente del sitio web funcione correctamente. Es la parte del sitio web que no puede ver ni con la que no puede interactuar. Es la parte del software que no entra en contacto
+ directo con los usuarios. Los usuarios acceden indirectamente a las partes y características desarrolladas por los diseñadores de back-end a través de una aplicación de front-end. Las actividades, como escribir API, crear bibliotecas y trabajar con componentes del sistema sin interfaces de usuario o incluso
+  sistemas de programación científica, también se incluyen en el backend. 
+
+</p>
+
+  
+</body>
+</html>
+
